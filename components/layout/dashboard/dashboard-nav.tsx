0a590b30842bc0cfda0a22105d8c469935f58a81
wekananda <igustiagungvivekananda@gmail.com>
2022-06-03 00:07:01 +0700

    Using avatar image from firebase

--- components/layout/dashboard/dashboard-nav.tsx
+++ components/layout/dashboard/dashboard-nav.tsx
@@ -55,7 +55,7 @@ export function DashboardNav({ user }: Props) {
                 <img
                   alt=""
                   className="h-8 w-8 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  src={user?.photoURL}
                 />
               </Menu.Button>
             </div>
